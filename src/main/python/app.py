75509f0afec688894363478bcec2654077903fe6
Petr Korobeinikov <pkorobeinikov@gmail.com>
2016-03-06 13:37:49 +0300

    Flask with Jinja template.

--- src/main/python/app.py
+++ src/main/python/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import render_template
 import sys
 import platform
 
@@ -16,6 +17,12 @@ def hello_world():
     hello_world = "Hello World"
     return hello_world
 
+
+@app.route('/user/')
+@app.route('/user/<username>')
+def show_user_profile(username=None):
+    t = render_template('user.j2', username=username)
+    return t
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
